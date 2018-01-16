1e71ec8b274d6d29dc37ac27a00b8112dcaf0721
Sam <huangsaiy@hotmail.com>
2018-01-16 13:49:04 +1100

    add material UI.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # gongbi
-MEAN stack for photo share in a group.
+MEAN stack for todo list web app with progress bar.
+use semantic UI as UI.
+unzip the semantic UI folder then put into "angular-app/src/app/vendor/" path.
--- angular-app/src/main.ts
+++ angular-app/src/main.ts
@@ -1,12 +1,107 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { enableProdMode } from '@angular/core';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
-
+import './polyfills';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+  import {
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatStepperModule,
+  } from '@angular/material';
+  import {HttpModule} from '@angular/http';
+  import {CdkTableModule} from '@angular/cdk/table';
+  
+  @NgModule({
+    exports: [
+      CdkTableModule,
+      MatAutocompleteModule,
+      MatButtonModule,
+      MatButtonToggleModule,
+      MatCardModule,
+      MatCheckboxModule,
+      MatChipsModule,
+      MatStepperModule,
+      MatDatepickerModule,
+      MatDialogModule,
+      MatExpansionModule,
+      MatGridListModule,
+      MatIconModule,
+      MatInputModule,
+      MatListModule,
+      MatMenuModule,
+      MatNativeDateModule,
+      MatPaginatorModule,
+      MatProgressBarModule,
+      MatProgressSpinnerModule,
+      MatRadioModule,
+      MatRippleModule,
+      MatSelectModule,
+      MatSidenavModule,
+      MatSliderModule,
+      MatSlideToggleModule,
+      MatSnackBarModule,
+      MatSortModule,
+      MatTableModule,
+      MatTabsModule,
+      MatToolbarModule,
+      MatTooltipModule,
+    ]
+  })
+  export class DemoMaterialModule {}
+  
+  @NgModule({
+    imports: [
+      BrowserModule,
+      BrowserAnimationsModule,
+      FormsModule,
+      HttpModule,
+      DemoMaterialModule,
+      MatNativeDateModule,
+      ReactiveFormsModule,
+    ],
+    providers: []
+  })
+  export class MaterialModule {}
+  
+  if (environment.production) {
+    enableProdMode();
+  }
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+  
+  /**  Copyright 2017 Google Inc. All Rights Reserved.
+      Use of this source code is governed by an MIT-style license that
+      can be found in the LICENSE file at http://angular.io/license */
