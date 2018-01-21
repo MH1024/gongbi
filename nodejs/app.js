eec1c8fe6d8839cc1712b5c90264d6df6274cba7
Sam <huangsaiy@hotmail.com>
2018-01-21 19:40:42 +1100

    add user control and change api.

--- nodejs/app.js
+++ nodejs/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var task = require('./routes/task');
 var settings =require('./settings');
 var app = express();
 
--- nodejs/routes/index.js
+++ nodejs/routes/index.js
@@ -2,8 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index', { title: 'HOME' });
+});
+router.get('/reg',function (req,res) {
+  res.render('reg',{title: 'REGISTRATION'})
+});
+router.post('/reg',function (reg,res) {
+});
+router.get('/login',function (req,res) {
+  res.render('login',{title: 'LOGIN'})
+});
+router.post('/login',function (req,res) {
+});
+router.get('/post',function (req,res) {
+  res.render('post',{title: 'CREATE'})
+});
+router.post('/post',function (req,res) {
+});
+router.get('/logout',function (req,res) {
 });
 
 module.exports = router;
